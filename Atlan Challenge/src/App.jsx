0c475bc33b8c1cab77a26e9d594cf88b74850f6a
rk1091 <riya_kapoor@hotmail.com>
2023-12-08 06:59:50 +0530

    added queryBuilder
    multiple results w. multiple queries
    
    UI changes

--- Atlan Challenge/src/App.jsx
+++ Atlan Challenge/src/App.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PastSessions from "./components/PastSession/PastSessions";
 import Result from "./components/Result/Result";
 import useSessions from "./hooks/useSesstion";
@@ -6,6 +6,10 @@ import "./index.css";
 import { ResultMap } from "./util/data";
 
 function App() {
+  const [fli, setFli] = useState(false);
+  const [count, setCount] = useState(0);
+  const [queryBuilder, setQueryBuilder] = useState("");
+  const [flip, setFlip] = useState(false);
   const {
     sessions,
     addSession,
@@ -15,12 +19,31 @@ function App() {
     setCurrentSessionIndex,
   } = useSessions();
 
+  // useEffect(() => {
+  //   // Update count when the query changes
+  //   const c = queryBuilder.split(";").length - 1;
+  //   setCount(c > 0 ? c : 0);
+  // }, [flip2]);
+
+  const onQueryBuilderChange = (e) => {
+    setQueryBuilder(e.target.value);
+    // setQueryBuilder(queryBuilder);
+  };
   const onQueryChange = (e) => {
     const id = sessions[currentSessionIndex].id;
     if (!id) {
       console.error("no current session");
       return;
     }
+    // let c = 0;
+    // console.log("HI");
+    // for (let i = 0; i < e.target.value.length; i++) {
+    //   if (e.target.value[i] == ";") c++;
+    // }
+    // console.log(c);
+    // setCount(c);
+    const c = e.target.value.split(";").length - 1;
+    setCount(c > 0 ? c : 0);
     updateSession(id, { query: e.target.value });
   };
 
@@ -29,6 +52,7 @@ function App() {
   }, [sessions, currentSessionIndex]);
 
   const runQuery = () => {
+    setFli((e) => !e);
     if (selectedSession.resultId) {
       return;
     }
@@ -39,6 +63,51 @@ function App() {
     updateSession(selectedSession.id, { resultId });
   };
 
+  const assignMainQuery = () => {
+    const id = sessions[currentSessionIndex].id;
+    if (!id) {
+      console.error("no current session");
+      return;
+    }
+
+    //logic to check Select from where group by having only space btw or chars if chars then append
+    const KEYWORDS = ["Select", "from", "where", "group", "by", "having"];
+    const words = queryBuilder.split(/\s+/);
+    let outputstring = "";
+    // console.log(words);
+    for (let i = 0; i < words.length - 1; i++) {
+      // if (words[i]=="having" && i<words.length)
+      if (KEYWORDS.includes(words[i]) && !KEYWORDS.includes(words[i + 1])) {
+        if (words[i] == "by") outputstring = outputstring + " group";
+        outputstring = outputstring + " " + words[i];
+        for (let j = i + 1; j < words.length; j++) {
+          if (KEYWORDS.includes(words[j])) break;
+          else outputstring = outputstring + " " + words[j];
+          // console.log(outputstring);
+        }
+      }
+      // else if (!KEYWORDS.includes(words[i]) && !KEYWORDS.includes(words[i + 1])) {
+      //   outputstring = outputstring + " " + words[i] + " " + words[i + 1];
+      //   console.log(outputstring);
+      // }
+
+      //   console.log(words[i]);
+      //   outputstring = outputstring;
+      // } else {
+      //   outputstring = outputstring + " " + words[i];
+      //   console.log(outputstring);
+    }
+    outputstring = outputstring.trim();
+    outputstring += ";\n";
+    // console.log(outputstring);
+
+    const qtext = sessions[currentSessionIndex].query;
+    // console.log(qtext);
+    if (outputstring !== "") {
+      if (qtext != "") updateSession(id, { query: qtext + outputstring });
+      else updateSession(id, { query: outputstring });
+    }
+  };
   return (
     <>
       <div className="vscode-container">
@@ -47,13 +116,11 @@ function App() {
           style={{
             display: "flex",
             flexDirection: "column",
-          }}
-        >
+          }}>
           <h2
             style={{
               alignSelf: "center",
-            }}
-          >
+            }}>
             SQL Editor
           </h2>
           <PastSessions
@@ -72,18 +139,66 @@ function App() {
                 Run Queries
               </button>
             </div>
-            <textarea
-              onChange={onQueryChange}
-              value={selectedSession?.query ?? ""}
-              className="um"
-              placeholder="Click a session to display or start typing queries to create a session..."
-            ></textarea>
+            <div className="mainArea">
+              <textarea
+                onChange={onQueryChange}
+                value={selectedSession?.query ?? ""}
+                className="mainQuery"
+                placeholder="Click a session to display or start typing queries to create a session..."></textarea>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}>
+                <center style={{ margin: "10px" }}>QueryBuilder:</center>
+                <textarea
+                  // style={{
+                  //   // display: "flex",
+                  //   // flexWrap: "wrap",
+                  //   // textOverflow: "clip",
+                  //   // overflowWrap: "break-word",
+
+                  //   overflowY: "hidden",
+                  //   // scr
+                  //   // textAlign: "justify",
+                  //   wordWrap: "break-word",
+                  //   // wordBreak: "break-all",
+                  //   // inlineSize: "100px",
+                  //   // overflowWrap: "breako-word",
+                  //   width: "100%",
+                  //   // flexDirection: "column",
+                  // }}
+                  className="queryBuilder"
+                  onChange={onQueryBuilderChange}
+                  onClick={() => {
+                    if (!flip) {
+                      setQueryBuilder("Select from where group by having");
+                      setFlip(true);
+                    }
+                  }}
+                  value={queryBuilder ?? ""}
+                  placeholder="Select --columnName-- from --tablename-- where --condition-- group by --colName-- having --colName--"></textarea>
+                <button
+                  className="default-button"
+                  style={{ marginTop: "10px" }}
+                  onClick={() => {
+                    // setQueryBuilder(queryBuilder);
+                    // onQueryChange()
+                    assignMainQuery();
+                  }}>
+                  ok!
+                </button>
+              </div>
+            </div>
           </div>
           <div className="editor2">
             {selectedSession &&
             selectedSession.resultId &&
             ResultMap[selectedSession.resultId] ? (
-              <Result data={ResultMap[selectedSession.resultId]} />
+              Array.from({ length: count }).map((_, i) => (
+                // for (let i = 0; i < count; i++) {
+                <Result key={i} data={ResultMap[selectedSession.resultId]} />
+              ))
             ) : (
               <div> No data</div>
             )}
--- Atlan Challenge/src/components/Result/Result.jsx
+++ Atlan Challenge/src/components/Result/Result.jsx
@@ -1,14 +1,16 @@
 import { useMemo } from "react";
+import "./index.css";
 
 export default function Result({ data }) {
+  const randomIndex = Math.floor(Math.random() * 3);
   const rows = useMemo(() => {
-    return Object.keys(data[0]);
+    return Object.keys(data[randomIndex]);
   });
 
   return (
     <div>
-      <table>
-        <thead>
+      <table className="table">
+        <thead className="tableHead">
           <tr>
             {rows.map((row) => (
               <th key={row}>{row}</th>
